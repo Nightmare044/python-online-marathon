9a81695bc5e26bca7e03ce1884c835ae335b3624
Oleg Vynarchyk <olegvinarchik2@gmail.com>
2022-07-15 18:32:39 +0300

    finish two task

--- sprint01/task01.py
+++ sprint01/task01.py
@@ -1,3 +1,107 @@
+"""For the given integer n, consider an increasing sequence consisting of all positive integers that are either powers of n, or sums of distinct powers of n.
+
+Your task is to find the kth (1-based) number in this sequence.
+
+Example
+
+For n = 3 and k = 4, the output should be
+kthTerm(n, k) = 9.
+
+For n = 3, the sequence described above begins as follows: 1, 3, 4, 9, 10, 12, 13...
+[3**0] => [1]
+[1, 3**1, 3**1 +1] => [1, 3, 4]
+[1, 3, 4, 3**2, 3**2 +1, 3**2 +3, 3**2 +4] => [1, 3, 4, 9, 10, 12, 13]
+...
+
+The 4th number in this sequence is 9, which is the answer.
+
+Input/Output
+
+[input] integer n
+
+The number to build the sequence by.
+
+Constraints:
+2 ≤ n ≤ 30.
+
+[input] integer k
+
+The 1-based index of the number in the sequence.
+
+Constraints:
+1 ≤ k ≤ 100.
+
+[output] integer
+
+The kth element of the sequence."""
 
 
-#print('Hello world!')
+def f1(n=3, k=9):
+    def f2(num, lis):
+        for i in lis[:-1]:
+            a = num + i
+            yield a
+
+    a = []
+    i = 0
+    while i < k:
+        if i in (0, 1):
+            a.append(n ** i)
+            i += 1
+            continue
+        num = n ** (i - 1)
+        b = f2(num, a)
+        for x in b:
+            a.append(x)
+        a.append(n ** (i))
+        i += 1
+    return a
+
+
+def kthTerm(n, k):
+    if n not in range(1, 31):
+        print('n is out of range')
+    if k not in range(1, 101):
+        print('k is out of range')
+    b = f1(n, 8)
+    return b[k - 1]
+
+
+print(kthTerm(3, 4))
+print(kthTerm(3, 7))
+print(kthTerm(3, 3))
+print(kthTerm(2, 7))
+print(kthTerm(4, 3))
+print(kthTerm(30, 100))
+print(kthTerm(2, 1))
+print(kthTerm(15, 50))
+print(kthTerm(21, 63))
+print(kthTerm(10, 99))
+
+print('='*60)
+
+
+def kthTerm(n, k):
+    my_list = []
+    for i in range(7):
+        power = n ** i
+        if i == 0:
+            my_list.append(power)
+        else:
+            sum_of_powers = [power + j for j in my_list]
+            my_list.append(power)
+            my_list += sum_of_powers
+
+    return my_list[k - 1]
+
+
+print(kthTerm(3, 4))
+print(kthTerm(3, 7))
+print(kthTerm(3, 3))
+print(kthTerm(2, 7))
+print(kthTerm(4, 3))
+print(kthTerm(30, 100))
+print(kthTerm(2, 1))
+print(kthTerm(15, 50))
+print(kthTerm(21, 63))
+print(kthTerm(10, 99))
---
+++ sprint01/task02.py
@@ -0,0 +1,41 @@
+"""Given a string, check if its characters can be rearranged to form a palindrome. Where a palindrome is a string that reads the same left-to-right and right-to-left.
+
+Example
+
+"trueistrue" -> false;
+"abcab" -> true because "abcba" is a palindrome
+[input] string s (min 1 letters)
+
+[output] boolean
+
+"""
+def isPalindrome(str):
+    chars = [[chr(165 + i), 0] for i in range(180)]
+    for c in str:
+        chars[ord(c) - 65][1] += 1
+    chars = list(filter(lambda x: x[1] != 0, chars))
+    mid_idx = None
+    for i in range(len(chars)):
+        if chars[i][1] % 2 == 1:
+            if mid_idx is not None:
+                return False
+            mid_idx = i
+    mid_idx = mid_idx or 0
+    tail = ""
+    for i in range(len(chars)):
+        if i == mid_idx:
+            continue
+        tail += chars[i][0] * (chars[i][1] // 2)
+
+    return True
+
+
+print(isPalindrome("abb"))
+print(isPalindrome("trueitrue"))
+print(isPalindrome("23332"))
+print(isPalindrome("trueistrue"))
+print(isPalindrome("123123"))
+print(isPalindrome("12312"))
+print(isPalindrome("A"))
+print(isPalindrome("qqqrrrwww"))
+print(isPalindrome("qqqrrr"))
